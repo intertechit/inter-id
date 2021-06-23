52204b6963572eb4fab308597e2b28b5190fe0d7
Barış Talha Güneş <60845583+baristalhagunes@users.noreply.github.com>
2021-06-23 15:43:15 +0300

    Layout eklendi, app sayfası oluşturuldu.

---
+++ client/src/pages/Rozetler.tsx
@@ -0,0 +1,401 @@
+import Layout from "../components/Layout";
+
+export default function Rozetler() {
+  return (
+    <Layout>
+      <div className="game-row">
+        <div className="rozet-box">
+          <div className="gem legerandy">
+            <img src="images/Legendary-icon.png" alt="" />
+          </div>
+
+          <div className="stars">
+            <ul>
+              <li>
+                <img src="images/star.png" alt="" />
+              </li>
+              <li>
+                <img src="images/star.png" alt="" />
+              </li>
+              <li>
+                <img src="images/star.png" alt="" />
+              </li>
+              <li>
+                <img src="images/star.png" alt="" />
+              </li>
+              <li>
+                <img src="images/star.png" alt="" />
+              </li>
+            </ul>
+          </div>
+
+          <div className="icon">
+            <img src="publishing-images/rozet/sample-icon.png" alt="" />
+          </div>
+
+          <div className="text">
+            <a href="#" data-toggle="modal" data-target="#rozetModal-01">
+              Lorem Ipsum
+            </a>
+          </div>
+        </div>
+
+        <div className="rozet-box">
+          <div className="gem unique">
+            <img src="images/unique-icon.png" alt="" />
+          </div>
+
+          <div className="stars">
+            <ul>
+              <li>
+                <img src="images/star.png" alt="" />
+              </li>
+              <li>
+                <img src="images/star.png" alt="" />
+              </li>
+            </ul>
+          </div>
+
+          <div className="icon">
+            <img src="publishing-images/rozet/sample-icon.png" alt="" />
+          </div>
+
+          <div className="text">
+            <a href="#" data-toggle="modal" data-target="#rozetModal-01">
+              Lorem Ipsum
+            </a>
+          </div>
+        </div>
+
+        <div className="rozet-box">
+          <div className="gem rare">
+            <img src="images/rare-icon.png" alt="" />
+          </div>
+
+          <div className="stars">
+            <ul>
+              <li>
+                <img src="images/star.png" alt="" />
+              </li>
+            </ul>
+          </div>
+
+          <div className="icon">
+            <img src="publishing-images/rozet/sample-icon.png" alt="" />
+          </div>
+
+          <div className="text">
+            <a href="#" data-toggle="modal" data-target="#rozetModal-01">
+              Lorem Ipsum
+            </a>
+          </div>
+        </div>
+
+        <div className="rozet-box">
+          <div className="gem uncommon">
+            <img src="images/uncommon-icon.png" alt="" />
+          </div>
+
+          <div className="stars">
+            <ul>
+              <li>
+                <img src="images/star.png" alt="" />
+              </li>
+              <li>
+                <img src="images/star.png" alt="" />
+              </li>
+              <li>
+                <img src="images/star.png" alt="" />
+              </li>
+              <li>
+                <img src="images/star.png" alt="" />
+              </li>
+              <li>
+                <img src="images/star.png" alt="" />
+              </li>
+            </ul>
+          </div>
+
+          <div className="icon">
+            <img src="publishing-images/rozet/sample-icon.png" alt="" />
+          </div>
+
+          <div className="text">
+            <a href="#" data-toggle="modal" data-target="#rozetModal-01">
+              Lorem Ipsum
+            </a>
+          </div>
+        </div>
+
+        <div className="rozet-box">
+          <div className="gem common">
+            <img src="images/common-icon.png" alt="" />
+          </div>
+
+          <div className="stars"></div>
+
+          <div className="icon">
+            <img src="publishing-images/rozet/sample-icon.png" alt="" />
+          </div>
+
+          <div className="text">
+            <a href="#" data-toggle="modal" data-target="#rozetModal-01">
+              Lorem Ipsum
+            </a>
+          </div>
+        </div>
+
+        <div className="rozet-box">
+          <div className="gem legerandy">
+            <img src="images/Legendary-icon.png" alt="" />
+          </div>
+
+          <div className="stars">
+            <ul>
+              <li>
+                <img src="images/star.png" alt="" />
+              </li>
+              <li>
+                <img src="images/star.png" alt="" />
+              </li>
+              <li>
+                <img src="images/star.png" alt="" />
+              </li>
+              <li>
+                <img src="images/star.png" alt="" />
+              </li>
+              <li>
+                <img src="images/star.png" alt="" />
+              </li>
+            </ul>
+          </div>
+
+          <div className="icon">
+            <img src="publishing-images/rozet/sample-icon.png" alt="" />
+          </div>
+
+          <div className="text">
+            <a href="#" data-toggle="modal" data-target="#rozetModal-01">
+              Lorem Ipsum
+            </a>
+          </div>
+        </div>
+
+        <div className="rozet-box">
+          <div className="gem legerandy">
+            <img src="images/Legendary-icon.png" alt="" />
+          </div>
+
+          <div className="stars">
+            <ul>
+              <li>
+                <img src="images/star.png" alt="" />
+              </li>
+              <li>
+                <img src="images/star.png" alt="" />
+              </li>
+              <li>
+                <img src="images/star.png" alt="" />
+              </li>
+              <li>
+                <img src="images/star.png" alt="" />
+              </li>
+              <li>
+                <img src="images/star.png" alt="" />
+              </li>
+            </ul>
+          </div>
+
+          <div className="icon">
+            <img src="publishing-images/rozet/sample-icon.png" alt="" />
+          </div>
+
+          <div className="text">
+            <a href="#" data-toggle="modal" data-target="#rozetModal-01">
+              Lorem Ipsum
+            </a>
+          </div>
+        </div>
+
+        <div className="rozet-box">
+          <div className="gem unique">
+            <img src="images/unique-icon.png" alt="" />
+          </div>
+
+          <div className="stars">
+            <ul>
+              <li>
+                <img src="images/star.png" alt="" />
+              </li>
+              <li>
+                <img src="images/star.png" alt="" />
+              </li>
+            </ul>
+          </div>
+
+          <div className="icon">
+            <img src="publishing-images/rozet/sample-icon.png" alt="" />
+          </div>
+
+          <div className="text">
+            <a href="#" data-toggle="modal" data-target="#rozetModal-01">
+              Lorem Ipsum
+            </a>
+          </div>
+        </div>
+
+        <div className="rozet-box">
+          <div className="gem rare">
+            <img src="images/rare-icon.png" alt="" />
+          </div>
+
+          <div className="stars">
+            <ul>
+              <li>
+                <img src="images/star.png" alt="" />
+              </li>
+            </ul>
+          </div>
+
+          <div className="icon">
+            <img src="publishing-images/rozet/sample-icon.png" alt="" />
+          </div>
+
+          <div className="text">
+            <a href="#" data-toggle="modal" data-target="#rozetModal-01">
+              Lorem Ipsum
+            </a>
+          </div>
+        </div>
+
+        <div className="rozet-box">
+          <div className="gem uncommon">
+            <img src="images/uncommon-icon.png" alt="" />
+          </div>
+
+          <div className="stars">
+            <ul>
+              <li>
+                <img src="images/star.png" alt="" />
+              </li>
+              <li>
+                <img src="images/star.png" alt="" />
+              </li>
+              <li>
+                <img src="images/star.png" alt="" />
+              </li>
+              <li>
+                <img src="images/star.png" alt="" />
+              </li>
+              <li>
+                <img src="images/star.png" alt="" />
+              </li>
+            </ul>
+          </div>
+
+          <div className="icon">
+            <img src="publishing-images/rozet/sample-icon.png" alt="" />
+          </div>
+
+          <div className="text">
+            <a href="#" data-toggle="modal" data-target="#rozetModal-01">
+              Lorem Ipsum
+            </a>
+          </div>
+        </div>
+
+        <div className="rozet-box">
+          <div className="gem common">
+            <img src="images/common-icon.png" alt="" />
+          </div>
+
+          <div className="stars"></div>
+
+          <div className="icon">
+            <img src="publishing-images/rozet/sample-icon.png" alt="" />
+          </div>
+
+          <div className="text">
+            <a href="#" data-toggle="modal" data-target="#rozetModal-01">
+              Lorem Ipsum
+            </a>
+          </div>
+        </div>
+
+        <div className="rozet-box">
+          <div className="gem legerandy">
+            <img src="images/Legendary-icon.png" alt="" />
+          </div>
+
+          <div className="stars">
+            <ul>
+              <li>
+                <img src="images/star.png" alt="" />
+              </li>
+              <li>
+                <img src="images/star.png" alt="" />
+              </li>
+              <li>
+                <img src="images/star.png" alt="" />
+              </li>
+              <li>
+                <img src="images/star.png" alt="" />
+              </li>
+              <li>
+                <img src="images/star.png" alt="" />
+              </li>
+            </ul>
+          </div>
+
+          <div className="icon">
+            <img src="publishing-images/rozet/sample-icon.png" alt="" />
+          </div>
+
+          <div className="text">
+            <a href="#" data-toggle="modal" data-target="#rozetModal-01">
+              Lorem Ipsum
+            </a>
+          </div>
+        </div>
+
+        <div className="rozet-box">
+          <div className="gem legerandy">
+            <img src="images/Legendary-icon.png" alt="" />
+          </div>
+
+          <div className="stars">
+            <ul>
+              <li>
+                <img src="images/star.png" alt="" />
+              </li>
+              <li>
+                <img src="images/star.png" alt="" />
+              </li>
+              <li>
+                <img src="images/star.png" alt="" />
+              </li>
+              <li>
+                <img src="images/star.png" alt="" />
+              </li>
+              <li>
+                <img src="images/star.png" alt="" />
+              </li>
+            </ul>
+          </div>
+
+          <div className="icon">
+            <img src="publishing-images/rozet/sample-icon.png" alt="" />
+          </div>
+
+          <div className="text">
+            <a href="#" data-toggle="modal" data-target="#rozetModal-01">
+              Lorem Ipsum
+            </a>
+          </div>
+        </div>
+
+        <div className="clear"></div>
+      </div>
+    </Layout>
+  );
+}
