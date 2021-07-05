72ef24fdbe8432299ae7f2b054368db35ee66319
Barış Talha Güneş <60845583+baristalhagunes@users.noreply.github.com>
2021-07-05 10:57:34 +0300

    Düzeltme

---
+++ backend/src/src/users/user.dto.ts
@@ -0,0 +1,26 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  surname: string;
+
+  @IsString()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  userName: string;
+}
+
+export class UpdateUserDto extends PartialType(CreateUserDto) {}
