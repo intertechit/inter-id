6f81b066ec0b667d89b283c3eff6fdcd245a3c52
Artun Çolak <artuncolak97@gmail.com>
2021-07-08 10:45:10 +0300

    yenio admin client eklendi

--- admin-client/src/components/layout/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <div className="xp-footerbar">
-      <footer className="footer">
-        <p className="mb-0">© Intertech 2021</p>
-      </footer>
-    </div>
-  );
-}
--- admin-client/src/pages/Deneme.tsx
+++
@@ -1,9 +0,0 @@
-import Layout from "../components/layout";
-
-export default function Deneme() {
-  return (
-    <Layout>
-      <h1>Deneme</h1>
-    </Layout>
-  );
-}
